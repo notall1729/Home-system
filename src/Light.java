30b50e310e31d7d4b34bf0bb8b322189a2438290
goli <shirafkanzeynab63@gmail.com>
2025-04-21 12:55:00 +0330

    update Light and Thermostat

--- src/Light.java
+++ src/Light.java
@@ -1,2 +1,7 @@
 public class Light extends Device{
+    private int brightness;
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
 }
--- src/Thermostat.java
+++ src/Thermostat.java
@@ -1,2 +1,7 @@
 public class Thermostat extends Device{
+    private int temperature;
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 }
