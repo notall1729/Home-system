1f629a4e68350680a5267d263d0ce061c1d3c14d
goli <shirafkanzeynab63@gmail.com>
2025-04-21 13:10:34 +0330

    add setProperty for Thermostat

--- src/Light.java
+++ src/Light.java
@@ -1,7 +1,12 @@
+import exception.InvalidCommandException;
+
 public class Light extends Device{
     private int brightness;
 
     public void setBrightness(int brightness) {
+        if(brightness < 0 || brightness > 100){
+            throw new InvalidCommandException("invalid value");
+        }
         this.brightness = brightness;
     }
 }
--- src/Thermostat.java
+++ src/Thermostat.java
@@ -1,7 +1,26 @@
+import exception.InvalidCommandException;
+
 public class Thermostat extends Device{
     private int temperature;
 
     public void setTemperature(int temperature) {
+        if(temperature > 30 || temperature < 10){
+            throw new InvalidCommandException("invalid value");
+        }
         this.temperature = temperature;
+    }
+
+    private boolean setProperty(String property, int value){
+        if(property.equals("temperature")){
+            setTemperature(value);
+        }
+
+        if (property.equals("brightness")){
+            throw new InvalidCommandException("invalid property");
+        }
+
+        if (property.equals("status")){
+            setStatus(value);
+        }
     }
 }
