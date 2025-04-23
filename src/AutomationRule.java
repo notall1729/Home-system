6fd7928362960cdb66d01c645f2d438c7a19202d
goli <shirafkanzeynab63@gmail.com>
2025-04-23 09:57:25 +0330

    add AutomationRule

---
+++ src/AutomationRule.java
@@ -0,0 +1,28 @@
+public class AutomationRule {
+    private String name;
+    private String time;
+    private String action;
+
+    public AutomationRule(String name, String time, String action) {
+        this.name = name;
+        this.time = time;
+        this.action = action;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + time + " " + action;
+    }
+}
